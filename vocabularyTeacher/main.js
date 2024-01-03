bd8ee6f10af51bc6687284f71bc2c1b44a042c6d
Juho H <Crare@users.noreply.github.com>
2024-01-03 18:04:28 +0200

    more work done on the testing phase and results view

--- vocabularyTeacher/main.js
+++ vocabularyTeacher/main.js
@@ -2,6 +2,7 @@ let lang1 = [];
 let lang2 = [];
 let vocabulary = [];
 let wordsLeftToCorrect = 0;
+let percentageDone = 0;
 let needToGetCorrectTimes = 5;
 let amountOfChoicesInMultiSelect = 4;
 
@@ -20,13 +21,13 @@ const createVocabulary = (lang1, lang2) => {
     };
     vocabulary.push(word);
   }
-  console.log("vocabulary", vocabulary);
+  // console.log("vocabulary", vocabulary);
   wordsLeftToCorrect = vocabulary.length;
 };
 
 const getRandomWord = () => {
   var randIndex = Math.floor(Math.random() * vocabulary.length);
-  console.log("getRandomWord randIndex", randIndex);
+  // console.log("getRandomWord randIndex", randIndex);
   return vocabulary[randIndex];
 };
 
@@ -38,24 +39,36 @@ const getRandomWordToTest = () => {
   var wordsToCorrectStill = vocabulary.filter(
     (v) => v.correctTimes < needToGetCorrectTimes
   );
-  console.log("wordsToCorrectStill", wordsToCorrectStill);
+  // console.log("wordsToCorrectStill", wordsToCorrectStill);
   wordsLeftToCorrect = wordsToCorrectStill.length;
   var randIndex = Math.floor(Math.random() * wordsLeftToCorrect);
-  console.log("randIndex", randIndex);
+  // console.log("randIndex", randIndex);
   wordToTest = wordsToCorrectStill[randIndex];
   wordToTestIndex = randIndex;
-  console.log("wordToTest", wordToTest);
+  // console.log("wordToTest", wordToTest);
 
   if (lastWord === wordToTest && wordsLeftToCorrect > 1) {
     getRandomWordToTest();
+  } else {
+    var totalCorrectTimesNeeded = vocabulary.length * needToGetCorrectTimes;
+    var correctTimesGet = 0;
+    for (var i = 0; i < vocabulary.length; i++) {
+      if (vocabulary[i].correctTimes > 0) {
+        correctTimesGet += vocabulary[i].correctTimes;
+      }
+    }
+    percentageDone = (correctTimesGet / totalCorrectTimesNeeded) * 100;
   }
 };
 
 const updateWordsInfo = () => {
   var txt = "Words: " + vocabulary.length;
   var txt2 = "words left to get correct: " + wordsLeftToCorrect;
+  var txt3 = " percentage done: " + percentageDone.toFixed(1) + "%";
   $("#words").text(txt);
   $("#wordsToCorrect").text(txt2);
+  $("#percentageDone").text(txt3);
+  $("#progressbar").width(percentageDone.toFixed(0) + "%");
 };
 
 const showContainer2 = () => {
@@ -63,39 +76,66 @@ const showContainer2 = () => {
   $("#container-2").show();
 };
 
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 const doMultiselectTest = () => {
   $("#multiselectTest").show();
   $("#writingTest").hide();
+
+  $("#seeRightAnswer").attr("disabled", false);
+  $("#rightAnswer").text("");
+  $("#result").text("");
+
+  removeButtonsFromMultiselect();
 
   $("#multiselectTestLang1").text(wordToTest.lang1 + " = ");
   var options = [];
 
   options.push(wordToTest.lang2);
-  console.log("amountOfChoicesInMultiSelect", amountOfChoicesInMultiSelect);
-  // while (options.length < amountOfChoicesInMultiSelect) {
-  var word = getRandomWord().lang2;
-  if (!options.includes(word)) {
-    console.log("not includes word ", word);
-    options.push(word);
+  // console.log("amountOfChoicesInMultiSelect", amountOfChoicesInMultiSelect);
+  while (options.length < amountOfChoicesInMultiSelect) {
+    var word = getRandomWord().lang2;
+    if (!options.includes(word)) {
+      // console.log("not includes word ", word);
+      options.push(word);
+    }
   }
-  // }
-  console.log("options", options);
-  // TODO: suffle options so right answer is not always first option.
+  shuffleArray(options);
+  // console.log("options", options);
 
   var btns = [];
   for (var i = 0; i < options.length; i++) {
     var button =
       '<button id="option_' +
       i +
-      '" type="button" class="btn btn-primary me-2">' +
+      '" type="button" class="multiselect-option btn btn-primary me-2">' +
       options[i] +
       "</button>";
     btns.push(button);
   }
+
   for (var i = 0; i < btns.length; i++) {
     $("#multiselectTestLang2Choices").append(btns[i]);
   }
+
+  $(".multiselect-option").on("click", (e) => {
+    $(".multiselect-option").attr("disabled", true);
+    var answer = e.target.innerHTML;
+    // console.log("answer: ", answer);
+    if (answer === wordToTest.lang2) {
+      answeredRight();
+    } else {
+      answeredWrong();
+    }
+  });
 };
+
+const disableOptions = () => {};
 
 const doWritingTest = () => {
   $("#writingTest").show();
@@ -124,9 +164,61 @@ const showResults = () => {
   $("#container-1").hide();
   $("#container-2").hide();
 
-  var txt = "results go here. TODO!";
+  var txt = "results:";
 
-  $("#testResults").text(txt);
+  txt += "\n\n";
+  txt +=
+    '<table class="table"><thead>' +
+    "<th>lang 1</th>" +
+    "<th>lang 2</th>" +
+    "<th>correct times</th>" +
+    "<th>incorrect times</th>" +
+    "<th>lookup answer</th>" +
+    "<th>score</th>" +
+    "</thead><tbody>";
+  for (let i = 0; i < vocabulary.length; i++) {
+    var score =
+      vocabulary[i].correctTimes -
+      vocabulary[i].incorrectTimes -
+      vocabulary[i].checkedAnswerTimes;
+    txt += "<tr>";
+    txt += "<td>" + vocabulary[i].lang1 + "</td>";
+    txt += "<td>" + vocabulary[i].lang2 + "</td>";
+    txt += "<td>" + vocabulary[i].correctTimes + "</td>";
+    txt += "<td>" + vocabulary[i].incorrectTimes + "</td>";
+    txt += "<td>" + vocabulary[i].checkedAnswerTimes + "</td>";
+    txt += "<td>" + score + "</td>";
+    txt += "</tr>";
+  }
+  txt += "</tbody></table>";
+
+  $("#testResults").empty();
+  $("#testResults").append(txt);
+};
+
+const answeredRight = () => {
+  wordToTest.correctTimes += 1;
+  $("#result").empty();
+  $("#result").append('<p style="color: green;">Answer was right.</p>');
+
+  vocabulary[wordToTestIndex] = wordToTest;
+
+  goToNextTest();
+};
+
+const answeredWrong = () => {
+  wordToTest.incorrectTimes += 1;
+  $("#wrongAnswer").show();
+  $("#result").empty();
+  $("#result").append(
+    '<p style="color: red;">Answer was wrong.\nRight answer is: ' +
+      wordToTest.lang2 +
+      "</p>"
+  );
+
+  vocabulary[wordToTestIndex] = wordToTest;
+
+  goToNextTest();
 };
 
 const doRandomTest = () => {
@@ -143,6 +235,10 @@ const doRandomTest = () => {
   } else {
     showResults();
   }
+};
+
+const removeButtonsFromMultiselect = () => {
+  $("#multiselectTestLang2Choices").empty();
 };
 
 $("#startTestingButton").on("click", function () {
@@ -167,8 +263,8 @@ $("#startTestingButton").on("click", function () {
     lang2 = [];
     return;
   }
-  console.log("lang1", lang1);
-  console.log("lang2", lang2);
+  // console.log("lang1", lang1);
+  // console.log("lang2", lang2);
   createVocabulary(lang1, lang2);
   doRandomTest();
 });
@@ -178,34 +274,30 @@ $("#clearListButton").on("click", function () {
   $("#2_lang_words").val("");
   lang1 = [];
   lang2 = [];
-  console.log("lang1", lang1);
-  console.log("lang2", lang2);
+  // console.log("lang1", lang1);
+  // console.log("lang2", lang2);
 });
 
 const goToNextTest = () => {
-  secondsLeft = 3;
+  secondsLeft = 2;
   showContinuinInSeconds();
-
-  setTimeout(() => {
-    secondsLeft = 2;
-    showContinuinInSeconds();
-  }, 1000);
 
   setTimeout(() => {
     secondsLeft = 1;
     showContinuinInSeconds();
-  }, 2000);
+  }, 1000);
 
   setTimeout(() => {
-    doRandomTest();
     secondsLeft = 0;
     hideContinuinInSeconds();
-  }, 3000);
+    doRandomTest();
+  }, 2000);
 };
 
 $("#seeRightAnswer").on("click", function () {
   $("#seeRightAnswer").attr("disabled", true);
   $("#sendAnswer").attr("disabled", true);
+  removeButtonsFromMultiselect();
 
   wordToTest.checkedAnswerTimes += 1;
   $("#rightAnswer").text(wordToTest.lang2);
@@ -219,17 +311,10 @@ const sendAnswer = () => {
 
   const answer = $("#writingTestLang2").val();
   if (answer === wordToTest.lang2) {
-    wordToTest.correctTimes += 1;
-    $("#result").text("Answer was right.");
+    answeredRight();
   } else {
-    wordToTest.incorrectTimes += 1;
-    $("#wrongAnswer").show();
-    $("#result").text("Answer was wrong. Right answer is: " + wordToTest.lang2);
+    answeredWrong();
   }
-
-  vocabulary[wordToTestIndex] = wordToTest;
-
-  goToNextTest();
 };
 
 $("#sendAnswer").on("click", function () {
